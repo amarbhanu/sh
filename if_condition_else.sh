7d7d6780f2d57aa4bb5e644daa718b4278c33490
Amar kumar Bhanu <amarkumar.bhanu@extramarks.com>
2018-11-01 13:07:17 +0530

    update

---
+++ if_condition_else.sh
@@ -0,0 +1,11 @@
+echo "Please enter type of fruit"
+read fruit
+
+if [ $fruit = apple ]
+        then echo "Good, I like Apples"
+elif [ $fruit = pear ]
+        then echo "Good, I like Pears"
+elif [ $fruit = banana ]
+        then echo "Good, I like Bananas"
+        else echo "Oh no, I hate Oranges!"
+fi
